b44a103c5c085b251e680c77b4c7767ffad10763
alianjum-web <muhammadalikhanzada7890@gmail.com>
2025-01-07 10:04:17 +0500

    add cads are added

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-function createCard(title, cName,  thumbnail) {
-    // Finish this function
-   
-    let html = `<div class="card">
-    <div class="image">
-        <img src="${thumbnail}"
-            alt="">
-        <div class="capsule">${duration}</div>
-    </div>
-    <div class="text">
-        <h1>${title}</h1>
-        <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
-    </div> 
-</div>`
+function createCard(img,pageLink, title, content) {
+  
+    let html = ` <section class="card">
+                    <div class="image">
+                        <img src="${img}">
+                    </div>
+                    <div class="text">
+                         <a href="${pageLink}">${title}</a>
+                        <p>
+                           ${content}
+                        </p>
+                    </div>
 
-    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
+                </section>`
+
+    
+    document.querySelector(".project-card").innerHTML = document.querySelector(".project-card").innerHTML + html;
 }
 
 
-createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")
+createCard("images/vanlife.png", "https://vanlife-prime.netlify.app/", "Vanlife App", "Rent the perfect van to make your perfect road trip.You pay for the van depending on the number of days.There are many vans available on the webpage.You can select your favorite one. ");
+
+createCard("images/giftlink.png", "https://gift-seven-smoky.vercel.app/", "GiftLink: A Smart Gift Idea", "Gave gifts to your loved ones.It uses sentiment analysis to suggest personalized gift ideas based on the recipient's details.  It showcases my full-stack development skills and ability to create practical, user-friendly applications.");
 
 /*
 
